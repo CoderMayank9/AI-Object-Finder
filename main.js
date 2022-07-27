2e2739d59ca37dcd550e38e498446d1874e4dc9c
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-07-27 18:28:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+objects=[];
+status="";
 function setup()
 {
 canvas=createCanvas(480,380);
 canvas.center();
+video=createCapture(VIDEO);
+video.size(480,380);
+video.hide();
+}
+function start()
+{
+objectDetector=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("status").innerHTML="Status: Detecting Objects";
+objectName=document.getElementById("object_name").value;
+}
+function modelloaded()
+{
+console.log("Model is Loaded");
+status=true;
+}
+function draw()
+{
+image(video,0,0,480,380);
 }
