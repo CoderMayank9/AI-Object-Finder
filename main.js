a933ed3ecc8dae4ff04a6cae320ff7a48f0b6da5
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-08-01 18:41:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,42 @@ function modelloaded()
 console.log("Model is Loaded");
 status=true;
 }
+function gotresults(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+console.log(results);
+objects=results;
+}
 function draw()
 {
 image(video,0,0,480,380);
+if(status!="")
+{
+objectDetector.detect(video,gotresults);
+for(i=0;i<objects.length;i++)
+{
+document.getElementById("status").innerHTML="Status: Objects Detected";
+fill(255,0,0);
+percent=floor(objects[i].confidence*100);
+text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+noFill();
+stroke(255,0,0);
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+if(objects[i].label==objectName)
+{
+objectDetector.detect(gotresults);
+document.getElementById("status").innerHTML=objectName+" Found";
+synth=window.speechSynthesis;
+utterThis=new SpeechSynthesisUtterance(objectName+"Found");
+synth.speak(utterThis);
+}
+else
+{
+document.getElementById("status").innerHTML=objectName+" Not Found";
+}
+}
+}
 }
